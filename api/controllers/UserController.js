ee777f73d4686635e460351a16280853d7295639
sohamkamani <soham_chetan_kamani@external.mckinsey.com>
2015-07-05 09:37:43 +0530

    imporved user api

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,15 +7,22 @@
 
 module.exports = {
 
-  addBillForUser : function(req, res){
+  addBillForUser: function (req, res) {
     var userId = req.body.userId,
       billId = req.body.billId;
 
-    User.find({userId:userId}, function(err, found){
+    console.log('phone is sending:' ,billId);
+
+    User.findOne({userId: userId}, function (err, found) {
       var foundBills = found.bills || [];
-      BillService.submitCurrentBill(function(err, data){
+      console.log('found bills : ', foundBills);
+      //BillService.submitCurrentBill(function(err, data){
+      //  foundBills.push(data);
+      //  User.update({userId:userId},{bills:foundBills}, console.log);
+      //});
+      Bill.findOne({id: billId}, function (err, data) {
         foundBills.push(data);
-        User.update({userId:userId},{bills:foundBills}, console.log);
+        User.update({userId: userId}, {bills: foundBills}, console.log);
       });
     });
 
